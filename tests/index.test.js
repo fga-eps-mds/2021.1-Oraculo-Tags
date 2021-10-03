5e758ed4640b012185b5bd446b6ccfb0629abf3c
Lucas Vieira <lucas.engen@outlook.com>
2021-10-03 02:54:45 -0300

    fga-eps-mds/2021.1-Oraculo#44 test: :white_check_mark: increases tests coverage
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,33 @@
 const app = require("../src/index");
+const request = require("supertest");
+const { initializeDatabase } = require("../src/Database");
+
+const validTag1 = {
+    name: "tag 1",
+    color: "#ff00ff",
+};
+
+const validTag2 = {
+    name: "tag 2",
+    color: "#ff00ff",
+};
 
 describe("Main tests", () => {
+    beforeAll(async () => {
+        await initializeDatabase();
+    });
+
     it("Check if we have express defined", async () => {
         expect(app).toBeDefined();
+    });
+
+    it("POST /tags - should create a tag", async () => {
+        const res = await request(app).post("/tags").send(validTag1);
+        expect(res.statusCode).toEqual(200);
+    });
+
+    it("POST /tags - should not create a tag with same color", async () => {
+        const res = await request(app).post("/tags").send(validTag2);
+        expect(res.statusCode).toEqual(500);
     });
 });
