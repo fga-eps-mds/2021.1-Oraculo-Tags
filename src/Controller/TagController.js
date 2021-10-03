2ce5447ca6fd9a8d12b4842d0d39391e5af39f5d
Lucas Vieira <lucas.engen@outlook.com>
2021-10-03 02:55:51 -0300

    fga-eps-mds/2021.1-Oraculo#44 fix: :bug: fixes lock when receiving a invalid request
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Controller/TagController.js
+++ src/Controller/TagController.js
@@ -3,12 +3,12 @@ const { Tag } = require("../Model/Tag");
 async function createTag(req, res) {
     const { name, color } = req.body;
 
-    const newTag = await Tag.create({ name, color });
-    if (newTag) {
+    try {
+        const newTag = await Tag.create({ name, color });
         return res.status(200).json(newTag);
+    } catch (err) {
+        return res.status(500).json({ error: "could not create a new tag. try again" });
     }
-
-    return res.status(500).json({ error: "could not create a new tag. try again" });
 }
 
 module.exports = { createTag };
