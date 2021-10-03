d89696ce536c915cd7f886dec26751eecdcfd1d7
Lucas Vieira <lucas.engen@outlook.com>
2021-10-03 01:42:00 -0300

    fga-eps-mds/2021.1-Oraculo#44 feat: :sparkles: adds tags database model - adds database configuration file
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

---
+++ src/Database/migrations/20211003043551-create-tags.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.createTable("tags", {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            name: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            color: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("tags");
+    },
+};
