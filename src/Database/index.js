dc8b30a289631b3049f5bcdee909e6d3f6908f75
Lucas Vieira <lucas.engen@outlook.com>
2021-10-03 02:35:03 -0300

    fga-eps-mds/2021.1-Oraculo#44 feat: :sparkles: implements database client setup
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

---
+++ src/Database/index.js
@@ -0,0 +1,30 @@
+const { Tag } = require("../Model/Tag");
+const config = require("./config");
+const { Sequelize } = require("sequelize");
+
+async function setupModels(db) {
+    Tag.init(db);
+}
+
+async function setupSequelize(config) {
+    return new Sequelize(config);
+}
+
+async function configure(auth, db) {
+    return new Promise((resolve, reject) => {
+        auth.then(() => {
+            setupModels(db);
+            resolve(0);
+        });
+    });
+}
+
+async function initializeDatabase() {
+    const db = await setupSequelize(config);
+    const auth = db.authenticate();
+    return configure(auth, db);
+}
+
+module.exports = {
+    initializeDatabase,
+};
